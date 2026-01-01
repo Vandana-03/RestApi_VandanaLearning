fcb0b7e36a1cf9a62fe57c8def92899d143bad44
Vandana-03 <testinggeekvandy@gmail.com>
2026-01-01 21:44:54 +0530

    testng plugin added

--- Dec25_java1/src/Dec25_2025/PurchaseAmountValidation.java
+++ Dec25_java1/src/Dec25_2025/PurchaseAmountValidation.java
@@ -1,11 +1,12 @@
 package Dec25_2025;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import files.payload_library;
 import io.restassured.path.json.JsonPath;
 
-public class PurchaseAmountValidation_TestNG {
+public class PurchaseAmountValidation {
 
 	//Verify if Sum of all Course prices matches with Purchase Amount	
 	@Test
@@ -13,6 +14,7 @@ public class PurchaseAmountValidation_TestNG {
 	{
 		JsonPath js= new JsonPath(payload_library.CoursePrice());
 		int course_count=js.getInt("courses.size()");
+		int purchase_amount=js.getInt("dashboard.purchaseAmount");
 		int sum = 0;
 		
 		for (int i = 0; i < course_count; i++) {
@@ -21,5 +23,6 @@ public class PurchaseAmountValidation_TestNG {
 		    sum = sum + (price * copiesSold);
 		}
 		System.out.println("Sum of all the course prices are " + sum);
+		Assert.assertEquals(sum, purchase_amount);
 	}
 }
