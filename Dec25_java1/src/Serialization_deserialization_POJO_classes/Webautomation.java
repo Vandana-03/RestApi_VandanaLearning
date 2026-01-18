f3a6f9fba890243c792a4ac5875156a33fd3e1c7
Vandana-03 <testinggeekvandy@gmail.com>
2026-01-18 06:23:41 +0530

    adding pojo classes

---
+++ Dec25_java1/src/Serialization_deserialization_POJO_classes/Webautomation.java
@@ -0,0 +1,22 @@
+package Serialization_deserialization_POJO_classes;
+
+public class Webautomation {
+	 private String course_title;
+	 private String price;
+	  
+  public String getCourse_title() {
+		return course_title;
+	}
+	public void setCourse_title(String course_title) {
+		this.course_title = course_title;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+ 
+  
+	
+}
